16ce952689b38851097569cd36c44b08a5954efe
lionardkevin <137513500+lionardkevin@users.noreply.github.com>
2023-06-23 20:39:43 +0700

    Delete testing directory

--- testing/Test_Block.py
+++
@@ -1,9 +0,0 @@
-import datetime as _dt
-import hashlib
-
-class Block:
-    def __init__(self, previous_hash, data):
-        self.data = data
-        self.previous_hash = previous_hash
-        string = "".join(data) + previous_hash
-        self.block_hash = hashlib.sha256((string).encode()).hexdigest()
